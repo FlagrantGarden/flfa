af5989642e3895109984358022c2f631565a7e21
Michael T Lombardi <michael.t.lombardi@gmail.com>
2022-04-19 22:23:33 -0500

    (GH-8) Make display more dynamic to buffer size
    This commit updates the display method on the shared model to render
    the app view with more awareness of the size of the buffer to improve
    how the app displays in different terminals and no longer requiring
    terminals of a precise size.
    
    A future commit will need to address the sizing for the prompts.

--- pkg/flfa/tui/tui.go
+++ pkg/flfa/tui/tui.go
@@ -1,6 +1,8 @@
 package tui
 
 import (
+	"fmt"
+
 	"github.com/FlagrantGarden/flfa/pkg/flfa"
 	"github.com/FlagrantGarden/flfa/pkg/tympan/compositor"
 	"github.com/FlagrantGarden/flfa/pkg/tympan/printers/terminal"
@@ -36,13 +38,56 @@ func Title(subtitle string, width int, options ...terminal.Option) string {
 	return coloredBar
 }
 
+type Sizes struct {
+	Buffer  BoxSize
+	Content BoxSize
+	ViewBox BoxSize
+}
+
+type BoxSize struct {
+	Height int
+	Width  int
+}
+
+func (model *SharedModel) DisplaySizes(content string) (sizes Sizes) {
+	sizes.Buffer.Height = model.Height
+	sizes.Buffer.Width = model.Width
+	sizes.Content.Height = lipgloss.Height(content)
+	sizes.Content.Width = lipgloss.Width(content)
+	maxHeight := 60
+	maxWidth := 180
+	sizes.ViewBox.Width = sizes.Buffer.Width - 2
+	sizes.ViewBox.Height = sizes.Buffer.Height - 2
+	if sizes.ViewBox.Width > maxWidth {
+		sizes.ViewBox.Width = maxWidth
+	}
+	if sizes.ViewBox.Height > maxHeight {
+		sizes.ViewBox.Height = maxHeight
+	}
+	if sizes.Content.Width > sizes.ViewBox.Width-2 {
+		model.FatalError = fmt.Errorf(
+			"Too wide; buffer is %d and content is %d",
+			sizes.Buffer.Width, sizes.Content.Width)
+	}
+	if sizes.Content.Height > sizes.ViewBox.Height-2 {
+		model.FatalError = fmt.Errorf(
+			"Too tall; buffer is %d and content is %d",
+			sizes.Buffer.Height, sizes.Content.Height)
+	}
+	return sizes
+}
+
 func (model *SharedModel) Display(subtitle string, body string, options ...terminal.Option) (view string) {
-	title := Title(subtitle, model.Width-2)
-	body = lipgloss.Place(lipgloss.Width(body)+2, lipgloss.Height(body)+2, lipgloss.Left, lipgloss.Top, body)
+	if model.Width == 0 || model.Height == 0 {
+		return "loading"
+	}
+	sizes := model.DisplaySizes(body)
+	title := Title(subtitle, sizes.ViewBox.Width)
+	body = lipgloss.Place(sizes.Content.Width+2, sizes.Content.Height+2, lipgloss.Left, lipgloss.Top, body)
 	view = lipgloss.JoinVertical(lipgloss.Center, title, body)
-	view = lipgloss.Place(180, 40, lipgloss.Left, lipgloss.Top, view)
+	view = lipgloss.Place(sizes.ViewBox.Width, sizes.ViewBox.Height, lipgloss.Left, lipgloss.Top, view)
 	view = lipgloss.NewStyle().Border(lipgloss.DoubleBorder(), true).BorderForeground(lipgloss.Color("#948AE3")).Render(view)
-	return lipgloss.Place(model.Width, model.Height, lipgloss.Center, lipgloss.Center, view)
+	return lipgloss.Place(sizes.Buffer.Width, sizes.Buffer.Height, lipgloss.Center, lipgloss.Center, view)
 }
 
 func TerminalSettings(options ...terminal.Option) *terminal.Settings {
