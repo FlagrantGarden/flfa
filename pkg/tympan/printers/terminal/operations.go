42c1e2d17f3f05297ae0a9b850263bc9f3c1fe79
Michael T Lombardi <michael.t.lombardi@gmail.com>
2022-04-24 23:55:33 -0500

    (TYMPAN) Add conditional operations to terminal
    Prior to this commit, dynamic styles did not have a way to specify if they should
    only apply some operation conditionally, requiring dynamic styles for each possible
    case or to add style handling by the caller, defeating the purpose.
    
    This commit adds the concept of conditions to the terminal package, functions which
    have access to an instance of terminal Settings and must return true or false. They
    are used in the new conditional operations, which all take a condition as input and
    only apply the operation if the condition returns true.
    
    This commit adds some default conditions for developers to leverage flags.

--- pkg/tympan/printers/terminal/operations.go
+++ pkg/tympan/printers/terminal/operations.go
@@ -6,6 +6,92 @@ import "github.com/charmbracelet/lipgloss"
 // Used in the Apply* method of a Settings instance.
 type Operation func(settings *Settings, style lipgloss.Style) lipgloss.Style
 
+// Conditions are functions which use the settings to determine whether or not an
+// Operation should be performed on a dynamic style.
+type Condition func(settings *Settings) bool
+
+// Helper condition to check if a flag has a particular setting before applying an operation.
+// Returns false if the flag does not have the specified setting.
+func IfFlagIs(name string, setting Flag) Condition {
+	return func(settings *Settings) bool {
+		return settings.Flag(name) == setting
+	}
+}
+
+// Helper condition to check if a list of flags have the specified settings before applying an operation.
+// Loops over each flag in the map, returning false if any flag has the wrong setting.
+func IfFlagsAre(flags map[string]Flag) Condition {
+	return func(settings *Settings) bool {
+		for name, setting := range flags {
+			if settings.Flag(name) != setting {
+				return false
+			}
+		}
+
+		return true
+	}
+}
+
+// Helper condition to check if a flag is set to on before applying an operation.
+func IfFlagIsOn(name string) Condition {
+	return func(settings *Settings) bool {
+		return settings.FlagIsOn(name)
+	}
+}
+
+// Helper condition to check if all of the specified flags are set to on before applying an operation.
+// Returns false if any flag specified is set to off or is unset.
+func IfFlagsAreOn(names ...string) Condition {
+	return func(settings *Settings) bool {
+		for _, name := range names {
+			if !settings.FlagIsOn(name) {
+				return false
+			}
+		}
+		return true
+	}
+}
+
+// Helper condition to check if a flag is set to off before applying an operation.
+func IfFlagsIsOff(name string) Condition {
+	return func(settings *Settings) bool {
+		return settings.FlagIsOff(name)
+	}
+}
+
+// Helper condition to check if all of the specified flags are set to off before applying an operation.
+// Returns false if any flag specified is set to on or is unset.
+func IfFlagsAreOff(names ...string) Condition {
+	return func(settings *Settings) bool {
+		for _, name := range names {
+			if !settings.FlagIsOff(name) {
+				return false
+			}
+		}
+		return true
+	}
+}
+
+// Helper condition to check if a flag is unset before applying an operation.
+func IfFlagsIsUnset(name string) Condition {
+	return func(settings *Settings) bool {
+		return settings.FlagIsUnset(name)
+	}
+}
+
+// Helper condition to check if all of the specified flags are unset before applying an operation.
+// Returns false if any flag specified is set to on or off.
+func IfFlagsAreUnset(names ...string) Condition {
+	return func(settings *Settings) bool {
+		for _, name := range names {
+			if !settings.FlagIsUnset(name) {
+				return false
+			}
+		}
+		return true
+	}
+}
+
 // A helper operation to inherit values from the specified style and appliy
 // them to the dynamic style, overwriting existing definitions. Only values
 // explicitly set on the style in argument will be applied.
@@ -29,6 +115,19 @@ func InheritFromStyle(styleToInherit lipgloss.Style) Operation {
 	}
 }
 
+// As InheritFromStyle, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func InheritFromStyleConditionally(condition Condition, styleToInherit lipgloss.Style) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		if condition(settings) {
+			return style.Inherit(styleToInherit)
+		}
+
+		return style
+	}
+}
+
 // A helper operation to override with values from the specified style.
 // Overriding inverts the logic of inheritance, extending the overriding style with
 // any values from the base style which are not set on the overriding style.
@@ -43,6 +142,15 @@ func InheritFromStyle(styleToInherit lipgloss.Style) Operation {
 // settings in the overriding style are preferred over the base style and any non-conflicting
 // styles - in this case, color and italics - are merged from both.
 func OverrideWithStyle(overridingStyle lipgloss.Style) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		return overridingStyle.Copy().Inherit(style)
+	}
+}
+
+// As OverrideWithStyle, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func OverrideWithStyleConditionally(condition Condition, overridingStyle lipgloss.Style) Operation {
 	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
 		return overridingStyle.Copy().Inherit(style)
 	}
@@ -65,6 +173,19 @@ func InheritFromExtraStyle(name string) Operation {
 	}
 }
 
+// As InheritFromExtraStyle, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func InheritFromExtraStyleConditionally(condition Condition, name string) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		source, ok := settings.Styles.Extra[name]
+		if ok && condition(settings) {
+			return style.Inherit(source)
+		}
+		return style
+	}
+}
+
 // A helper operation to override with values from the specified extra style.
 // Overriding inverts the logic of inheritance, extending the overriding style with
 // any values from the base style which are not set on the overriding style.
@@ -72,6 +193,19 @@ func OverrideWithExtraStyle(name string) Operation {
 	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
 		source, ok := settings.Styles.Extra[name]
 		if ok {
+			return source.Copy().Inherit(style)
+		}
+		return style
+	}
+}
+
+// As OverrideWithExtraStyle, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func OverrideWithExtraStyleConditionally(condition Condition, name string) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		source, ok := settings.Styles.Extra[name]
+		if ok && condition(settings) {
 			return source.Copy().Inherit(style)
 		}
 		return style
@@ -90,11 +224,38 @@ func ColorizeBackground(name string) Operation {
 	}
 }
 
+// As ColorizeBackground, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func ColorizeBackgroundConditionally(condition Condition, name string) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		color, ok := settings.Colors.Extra[name]
+		if ok && condition(settings) {
+			return style.Background(color)
+		}
+
+		return style
+	}
+}
+
 // A helper operation to set the text's foreground with the specified extra color.
 func ColorizeForeground(name string) Operation {
 	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
 		color, ok := settings.Colors.Extra[name]
 		if ok {
+			return style.Foreground(color)
+		}
+		return style
+	}
+}
+
+// As ColorizeForeground, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func ColorizeForegroundConditionally(condition Condition, name string) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		color, ok := settings.Colors.Extra[name]
+		if ok && condition(settings) {
 			return style.Foreground(color)
 		}
 		return style
@@ -118,6 +279,37 @@ func ColorizeForeground(name string) Operation {
 // With more than four arguments nothing will be set.
 func ColorizeBorderBackground(names ...string) Operation {
 	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		if len(names) < 1 || len(names) > 4 {
+			return style
+		}
+
+		var colors []lipgloss.TerminalColor
+
+		for _, name := range names {
+			color, ok := settings.Colors.Extra[name]
+			if !ok {
+				return style
+			}
+
+			colors = append(colors, color)
+		}
+
+		if len(colors) != len(names) {
+			return style
+		}
+		return style.BorderBackground(colors...)
+	}
+}
+
+// As ColorizeBorderBackground, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func ColorizeBorderBackgroundConditionally(condition Condition, names ...string) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		if !condition(settings) {
+			return style
+		}
+
 		if len(names) < 1 || len(names) > 4 {
 			return style
 		}
@@ -180,11 +372,57 @@ func ColorizeBorderForeground(names ...string) Operation {
 	}
 }
 
+// As ColorizeBorderForeground, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func ColorizeBorderForegroundConditionally(condition Condition, names ...string) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		if !condition(settings) {
+			return style
+		}
+
+		if len(names) < 1 || len(names) > 4 {
+			return style
+		}
+
+		var colors []lipgloss.TerminalColor
+
+		for _, name := range names {
+			color, ok := settings.Colors.Extra[name]
+			if !ok {
+				return style
+			}
+
+			colors = append(colors, color)
+		}
+
+		if len(colors) != len(names) {
+			return style
+		}
+
+		return style.BorderForeground(colors...)
+	}
+}
+
 // A helper operation to set the border's bottom background with the specified extra color(s).
 func ColorizeBorderBottomBackground(name string) Operation {
 	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
 		color, ok := settings.Colors.Extra[name]
 		if ok {
+			return style.BorderBottomBackground(color)
+		}
+
+		return style
+	}
+}
+
+// As ColorizeBorderBottomBackground, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func ColorizeBorderBottomBackgroundConditionally(condition Condition, name string) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		color, ok := settings.Colors.Extra[name]
+		if ok && condition(settings) {
 			return style.BorderBottomBackground(color)
 		}
 
@@ -204,11 +442,39 @@ func ColorizeBorderLeftBackground(name string) Operation {
 	}
 }
 
+// As ColorizeBorderLeftBackground, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func ColorizeBorderLeftBackgroundConditionally(condition Condition, name string) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		color, ok := settings.Colors.Extra[name]
+		if ok && condition(settings) {
+			return style.BorderLeftBackground(color)
+		}
+
+		return style
+	}
+}
+
 // A helper operation to set the border's right background with the specified extra color(s).
 func ColorizeBorderRightBackground(name string) Operation {
 	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
 		color, ok := settings.Colors.Extra[name]
 		if ok {
+			return style.BorderRightBackground(color)
+		}
+
+		return style
+	}
+}
+
+// As ColorizeBorderRightBackground, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func ColorizeBorderRightBackgroundConditionally(condition Condition, name string) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		color, ok := settings.Colors.Extra[name]
+		if ok && condition(settings) {
 			return style.BorderRightBackground(color)
 		}
 
@@ -228,11 +494,39 @@ func ColorizeBorderTopBackground(name string) Operation {
 	}
 }
 
+// As ColorizeBorderTopBackground, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func ColorizeBorderTopBackgroundConditionally(condition Condition, name string) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		color, ok := settings.Colors.Extra[name]
+		if ok && condition(settings) {
+			return style.BorderTopBackground(color)
+		}
+
+		return style
+	}
+}
+
 // A helper operation to set the border's bottom foreground with the specified extra color(s).
 func ColorizeBorderBottomForeground(name string) Operation {
 	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
 		color, ok := settings.Colors.Extra[name]
 		if ok {
+			return style.BorderBottomForeground(color)
+		}
+
+		return style
+	}
+}
+
+// As ColorizeBorderBottomForeground, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func ColorizeBorderBottomForegroundConditionally(condition Condition, name string) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		color, ok := settings.Colors.Extra[name]
+		if ok && condition(settings) {
 			return style.BorderBottomForeground(color)
 		}
 
@@ -252,11 +546,39 @@ func ColorizeBorderLeftForeground(name string) Operation {
 	}
 }
 
+// As ColorizeBorderLeftForeground, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func ColorizeBorderLeftForegroundConditionally(condition Condition, name string) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		color, ok := settings.Colors.Extra[name]
+		if ok && condition(settings) {
+			return style.BorderLeftForeground(color)
+		}
+
+		return style
+	}
+}
+
 // A helper operation to set the border's right foreground with the specified extra color(s).
 func ColorizeBorderRightForeground(name string) Operation {
 	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
 		color, ok := settings.Colors.Extra[name]
 		if ok {
+			return style.BorderRightForeground(color)
+		}
+
+		return style
+	}
+}
+
+// As ColorizeBorderRightForeground, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func ColorizeBorderRightForegroundConditionally(condition Condition, name string) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		color, ok := settings.Colors.Extra[name]
+		if ok && condition(settings) {
 			return style.BorderRightForeground(color)
 		}
 
@@ -276,11 +598,38 @@ func ColorizeBorderTopForeground(name string) Operation {
 	}
 }
 
+// As ColorizeBorderTopForeground, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func ColorizeBorderTopForegroundConditionally(condition Condition, name string) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		color, ok := settings.Colors.Extra[name]
+		if ok && condition(settings) {
+			return style.BorderTopForeground(color)
+		}
+
+		return style
+	}
+}
+
 // A helper operation to replace the base style of the dynamic style instead of building
 // on the Primary style from settings; because operations are applied in the order they
 // are specified, this should always be the first operation passed in a list of operations.
 func SetBaseStyle(base lipgloss.Style) Operation {
 	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
 		return base.Copy()
+	}
+}
+
+// As SetBaseStyle, but only applied if the specified condition returns true.
+// Conditions are checked against the settings when using Apply or calling a DynamicStyle;
+// they don't need to be true when the condition is specified, only when it is checked.
+func SetBaseStyleConditionally(condition Condition, base lipgloss.Style) Operation {
+	return func(settings *Settings, style lipgloss.Style) lipgloss.Style {
+		if condition(settings) {
+			return base.Copy()
+		}
+
+		return style
 	}
 }
